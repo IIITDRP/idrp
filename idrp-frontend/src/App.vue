98417818f67c7007b063ad4292c02d72a0162b98
Shivshankar <shivbk56@gmail.com>
2026-08-07 10:58:30 +0530

    Removed speed insights

--- idrp-frontend/src/App.vue
+++ idrp-frontend/src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <AppLayout />
   <Analytics/>
-  <SpeedInsights />
 </template>
 
 <script setup lang="ts">
 import AppLayout from '@/components/AppLayout.vue'
 import { Analytics } from '@vercel/analytics/vue'
-import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
